21094638513013dde3f8c10aea71360a9cdb2da3
xlaoy <768881412@qq.com>
2018-04-18 16:14:24 +0800

    first commit

---
+++ src/main/java/com/xlaoy/sso/SSOApplaction.java
@@ -0,0 +1,21 @@
+package com.xlaoy.sso;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by Administrator on 2018/2/1 0001.
+ */
+@EnableSwagger2Doc
+@EnableEurekaClient
+@EnableCircuitBreaker
+@SpringBootApplication(scanBasePackages = "com.xlaoy")
+public class SSOApplaction {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SSOApplaction.class, args);
+    }
+}
