47df1778b3dd0a7974d3fa1afb85c1e068a8188c
xlaoy <768881412@qq.com>
2018-07-09 11:46:12 +0800

    redis

--- src/main/java/com/xlaoy/sso/SSOApplaction.java
+++ src/main/java/com/xlaoy/sso/SSOApplaction.java
@@ -3,13 +3,11 @@ package com.xlaoy.sso;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by Administrator on 2018/2/1 0001.
  */
 @EnableSwagger2Doc
-@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "com.xlaoy")
 public class SSOApplaction {
 
